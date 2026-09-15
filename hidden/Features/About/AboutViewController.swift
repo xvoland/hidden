edb2b017c0f2456b2eb52ea569572f99f6bade20
xVoLAnD <xvoland@gmail.com>
2026-09-15 14:58:27 +0300

    feat: show fork maintainer credit with link to dotoca.net in About window

--- hidden/Features/About/AboutViewController.swift
+++ hidden/Features/About/AboutViewController.swift
@@ -27,6 +27,27 @@ class AboutViewController: NSViewController {
         guard let version = Bundle.main.releaseVersionNumber,
                 let buildNumber = Bundle.main.buildVersionNumber else { return }
         lblVersion.stringValue += " \(version) (\(buildNumber))"
+        addMaintainerCredit()
     }
-    
+
+    // Fork maintainer credit with a clickable link to the maintainer's site.
+    private func addMaintainerCredit() {
+        let link = HyperlinkTextField()
+        link.href = "https://dotoca.net"
+        link.stringValue = "Maintained by Vitalii Tereshchuk (xVoLAnD)"
+        link.isEditable = false
+        link.isSelectable = false
+        link.isBordered = false
+        link.drawsBackground = false
+        link.font = NSFont.systemFont(ofSize: NSFont.smallSystemFontSize)
+        link.textColor = .secondaryLabelColor
+        link.alignment = .center
+        link.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(link)
+        NSLayoutConstraint.activate([
+            link.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            link.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32)
+        ])
+    }
+
 }
